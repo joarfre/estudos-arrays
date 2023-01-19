6d707b5668440807a7c31889d3eda002aa348992
Jonathan Freitas <jonathan_freitas@id.uff.br>
2023-01-19 18:19:15 -0300

    adicionando o arquivo 'lista-duas-dimensoes.js'

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,10 @@
+const alunos = ["Joao", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(`A aluna da posiçao 1 da lista de alunos é: ${
+    listaDeAlunosEMedias[0][1]
+}. E a sua nota é: ${
+    listaDeAlunosEMedias[1][1]
+}.`);
