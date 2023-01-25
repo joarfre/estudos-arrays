b0102795749e8726b8e85869f9e9069968d3cadb
Jonathan Freitas <jonathan_freitas@id.uff.br>
2023-01-25 17:48:22 -0300

    incrementando como calcular média em matrizesno arquivo 'media-for.js'

--- media-for.js
+++ media-for.js
@@ -9,3 +9,23 @@ for (let i = 0; i < notas.length; i++) {
 const media = somaDasNotas / notas.length;
 
 console.log(`A média das notas é ${media}.`)
+
+// Para matrizes, utiliza-se 1 for para cada dimensão
+
+const notas1 = [10, 6.5, 8 ,7.5]
+const notas2 = [9, 6, 6]
+const notas3 = [8.5, 9.5]
+
+const notasGerais = [notas1,notas2,notas3]
+
+let media2 = 0
+
+for (let i = 0; i < notasGerais.length; i++) {
+  for (let j = 0; j < notasGerais[i].length; j++) {
+    media2 += notasGerais[i][j]/notasGerais[i].length;
+  }
+}
+
+media2 = media2/notasGerais.length
+
+console.log(media2)
