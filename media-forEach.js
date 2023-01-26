1edc044d71293e019ef9cc57f5bf102f70918bf2
Jonathan Freitas <jonathan_freitas@id.uff.br>
2023-01-26 13:14:33 -0300

    adicionando o arquivo 'media-forEach.js'

---
+++ media-forEach.js
@@ -0,0 +1,16 @@
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0;
+
+notas.forEach(function (nota, indice) {
+    somaDasNotas += nota;
+    console.log(indice);
+});
+
+const media =somaDasNotas / notas.length;
+
+console.log(`A média das notas é ${media}.`);
+
+// o indice na função do forEach não é necessário,
+// está presente apenas para mostrar que pode ser passado
+//outros parâmetros
